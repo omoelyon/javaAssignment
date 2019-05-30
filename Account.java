53dddf5edb2beee902613dab7a2297fa7ec905c1
omoelyon <ganiyuelegbede@yahoo.com>
2019-05-30 08:46:52 +0100

    my working account assignment

--- Account.java
+++ Account.java
@@ -5,17 +5,6 @@
     private String name; // instance variable
     private double balance; // instance variable
 
-    // Account constructor that receives two parameters
-    public Account(String name, double balance) {
-    this.name = name; // assign name to instance variable name
-
-        // validate that the balance is greater than 0.0; if it's not,
-    // instance variable balance keeps its defau
-        if (balance > 0.0) { // if the balance is valid
-            this.balance = balance; // assign it to instance variable balanc
-            }
-        }
-            
      // method that deposits (adds) only a valid amount to the balance
      public void deposit(double depositAmount) {
         if (depositAmount > 0.0) { // if the depositAmount is valid
--- AccountTest.java
+++ AccountTest.java
@@ -3,22 +3,17 @@ import java.util.Scanner ;
 
 public class AccountTest {
 	public static void main(String[] args){
-	// create a scanner object to obtain input from the window
+
 	Scanner input = new Scanner(System.in);
 	
-	//Create an account object and assign it to myAccount
 	Account myAccount = new Account();
+
+	System.out.println("Please enter amount:"); 
+	int amount = input.nextInt();
+	myAccount.withdraw(amount);
+
+	System.out.println();
+
 	
-	// display initial value of name (null)
-	// System.out.printf("Initial name id: %s%n%n", myAccount.getName());
-
-	// prompt for and read name
-	System.out.println("Please enter the name:"); 
-	String theName = input.nextLine(); // read a line of text
-	myAccount.setName(theName); //putthe name in myAccount
-	System.out.println(); // output a blank line
-
-	// dsplay the name stored in object myAccount
-	System.out.printf("Name in object myAccount is:%n%s%n", myAccount.getName());
 	}
    }
